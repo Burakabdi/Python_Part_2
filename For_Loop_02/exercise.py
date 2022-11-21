86028b21165c41bf9c974a51154b0d381d0d6733
Burakabdi <burakabdiolu@gmail.com>
2022-11-21 23:17:42 +0300

    Complete_for_loop_02

---
+++ For_Loop_02/exercise.py
@@ -0,0 +1,6 @@
+todo = ["exercise1", "exercise2", "exercise3","coffee break" ,"exercise4","exercise5","exercise6"]
+
+for x in todo:
+    print(x)
+    if x.upper() == "COFFEE BREAK":
+        break
