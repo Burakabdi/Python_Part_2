eefaae2b33ca843d09cdeb60ec339251b2df17b9
Burakabdi <burakabdiolu@gmail.com>
2022-11-21 23:12:07 +0300

    Complete_exercise_04

---
+++ exercise-04/exercise.py
@@ -0,0 +1,7 @@
+names = ["John", "Tristram", "Baldwin", "Wally"]
+surnames = ["Doe", "Mcbride", "Preston", "Collins"]
+
+
+
+for name, surname in zip(names,surnames):
+    print(f"Hello {name} {surname}! ")
